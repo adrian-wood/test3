fcb42b12ff1f4e85a05fda8cde70659ac1113b6e
Andy Moorhouse <andrew.moorhouse@metoffice.gov.uk>
2016-11-29 10:17:17 +0000

    MB-1385 Create a monitoring page to combine the monitoring for the GRIB, DF and eventual XML storage

---
+++ metdb_utils/readme.md
@@ -0,0 +1,6 @@
+## metdb_utils folder
+=====================
+
+Put any useful or common functions, modules or packages in here.
+
+NB if adding a Python package, you need a `__init__.py` file in the package directory to make the `import` work. The file can be empty.
---
+++ metdb_utils/web/daily_archive.py
@@ -0,0 +1,35 @@
+def daily_archive(symLink,baseDir,page):
+  """Write a HTML page to a location based on the current date and create a symbolic link to it.
+
+  This will usually be used for scripts which run on a regular basis, creating
+  output that needs to be stored in an "archive" directory, with a link
+  to the "current" page.
+  The actual page will be created in a file named <dd>.html in subdirectories 
+  <yyyy>/<mm> under the baseDir argument - these will be created if necessary.
+  The symbolic link supplied in the symlink argument will then be destroyed and 
+  re-created, linking to the file written.
+  The contents of the page should be supplied in the "page" argument.
+
+  Args:
+    symLink: the fully-qualified path to the symlin to be re-created
+    baseDir: the fully-qualified base directory of the "archive"
+    page: the HTML output to be written to the file
+
+  Returns:
+    Nothing
+  """
+  import os
+  from datetime import datetime
+  now=datetime.now()
+  outDir=baseDir + now.strftime("/%Y/%m")
+
+  if not os.path.exists(outDir):
+    os.makedirs(outDir)
+
+  outFile=outDir + now.strftime("/%d") + ".html"
+
+  with open(outFile, "w") as outp:
+    outp.write(page)
+    # Recreate the symlink to the latest file
+    os.unlink(symLink)
+    os.symlink(outFile,symLink)
