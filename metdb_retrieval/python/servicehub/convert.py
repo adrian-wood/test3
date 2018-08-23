52d4fb2012f98f49ad3af2a063c338beebfe3bac
Sheila needham <sheila.needham@metoffice.gov.uk>
2018-08-23 16:01:18 +0100

    MB-1798: New coding for replicated elements; METARS retrievals, fixes for binary retrievals and changes to SREWS and RAINFALL as requested

--- metdb_retrieval/python/servicehub/convert.py
+++ metdb_retrieval/python/servicehub/convert.py
@@ -1,6 +1,3 @@
-""" .. module:: convert
-    .. moduleauthor:: Sheila Needham
-"""
 import numpy as np
 from unit_utils import *
 
@@ -9,6 +6,53 @@ from unit_utils import *
 # This section has all the individual conversion functions that can be
 # called from the elements table.
 # ---------------------------------------------------------------------
+
+# ---------------------------------------------------------------------
+def metar_temp(value):
+    """Convert temperature from K to degrees c.
+       Specific for METARS which are converted from C to K by MetDB
+       using 273K.
+       parameter: float temperature in K or MDI
+       returns: string temperature in degrees C or MDI
+    """
+    result = ""
+    if value is not MDI:
+        result = "{:.0f}".format(value - 273)
+    return result
+
+
+# ---------------------------------------------------------------------
+def synop_temp(value):
+    """Convert temperature from K to degrees c.
+       Specific for SYNOP/SHIP which are converted from C to K by MetDB
+       using 273.1K.
+       parameter: float temperature in K or MDI
+       returns: string temperature in degrees C and tenths, or MDI
+    """
+    result = ""
+    if value is not MDI:
+        result = "{:.1f}".format(value - 273.1)
+    return result
+
+
+# ---------------------------------------------------------------------
+def ship_id(id, callsign):
+    """Convert a ship/buoy ID from either number (BUOY_IDNY) or
+       SHIP (CALLSIGN).  Bug in the python metdb module means that
+       missing strings are not handled correctly so this will take
+       whatever value it finds in either slot! This only works because
+       these elements are mutually exclusive.  it should still work when
+       the metdb module is fixed.
+       parameters: integer - buoy identity
+                   string - ship callsign
+       returns: string of one or the other.
+    """
+    result = ""
+    if id is not MDI:
+        result = string(numeric)
+    elif callsign is not MDI:
+        result = callsign
+    return result
 
 
 # -----------------------------------------------------------------------
---
+++ metdb_retrieval/servicehub/get_03_srew_data.sh
@@ -0,0 +1,116 @@
+#!/bin/bash -l   
+
+#-----------------------------------------------------------------------
+#
+# SCRIPT        : get_srew_data.sh   
+#
+# PURPOSE       : Run python retrieval script to get SREW data for
+#                 ServiceHub. Block 03 only.
+#
+# CALLED BY     : moodsf cron  
+#
+# ARGUMENTS     : (1) config file
+#
+# REVISION INFO :
+#
+# MB-1790: Added FTP Step.                  
+#-----------------------------------------------------------------------
+# (C) CROWN COPYRIGHT 2018 - MET OFFICE. All Rights Reserved.
+#
+# Met Office, United Kingdom
+#
+# The use, duplication and disclosure of this code is strictly
+# prohibited without the permission of The Meteorological Database
+# Team at the above address.
+#-----------------------------------------------------------------------
+
+module load scitools
+module display scitools 
+
+. /var/moods/metdb_retrieval/servicehub/sendfile.sh
+
+# Check arguments
+if [[ $# -ne 1 ]]; then
+  echo "Usage: $0 <full_path_to_config_file>"
+  exit 8
+fi
+CONFIG=$1
+
+if [[ ! -s $CONFIG ]]; then
+  echo "Error: $CONFIG file not found"
+  exit 8
+fi
+
+# Get the base directory from the config file
+# (gets the line starting base_dir; gets the bit after the equals; trims white space)
+
+base_dir=$(grep ^base_dir $CONFIG | cut -d'=' -f2 | sed 's/^ *//;s/ *$//')
+
+
+# Get the output directory from the config file in a similar fashion
+output_dir=$(grep ^output_dir $CONFIG | cut -d'=' -f2 | sed 's/^ *//;s/ *$//')
+
+# Get the python path from the config file
+pypath=$(grep ^pythonpath $CONFIG | cut -d'=' -f2 | sed 's/^ *//;s/ *$//')
+echo 'pypath is '$pypath
+export PYTHONPATH=$PYTHONPATH:$pypath
+
+#
+# Run the retrieval
+#
+python $base_dir/python/get_data.py -c $CONFIG
+rc=$?
+
+if [[ $rc -ne 0 ]]; then
+  echo "Errors in retrieval"
+  mailx -s "ServiceHub SREW retrieval error" metdb@metoffice.gov.uk < $base_dir/sevicehub/email.txt
+  exit 8
+fi
+
+#
+# transfer output files to Cloud Transfer Service
+#
+CTS1=ssaftp01-zvopaph1
+CTS2=ssaftp02-zvopaph2
+DEST=srew-uk-csv
+
+num_files=$(ls -1 $output_dir/03_srew_data*.csv 2>/dev/null | wc -l)
+echo "$num_files files to transfer"
+
+#  ... check that there are some to copy
+
+if [ "$num_files" -gt 0 ]
+then
+
+# Copy one at a time - trying the secondary server if the first
+# one fails.
+
+  for infile in $output_dir/03_srew_data*.csv
+  do
+    temp=${infile##/*/}
+    outfile=${temp#*_}
+    sendfile $CTS1 $infile $DEST $outfile
+    rc=$?
+
+    if [ "$rc" -ne 0 ]
+    then
+      echo "trying secondary server"
+      sendfile $CTS2 $infile $DEST $outfile
+      rc=$?
+      if [ "$rc" -ne 0 ]
+      then
+        echo "FTP failed on both servers"
+      else
+      #  cp $infile /tmp/sent03/$outfile
+        rm $infile
+      fi
+    else
+    #  cp $infile /tmp/sent03/$outfile
+      rm $infile
+    fi
+    
+  done
+
+fi
+
+
