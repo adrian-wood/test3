c79ae00d44d3e7a986b9163f85052c9a4102998c
Andy Moorhouse <andrew.moorhouse@metoffice.gov.uk>
2019-01-07 16:08:46 +0000

    MB-1891 AIRQAL and AIRQALEU in ServiceHub

--- metdb_retrieval/python/get_data.py
+++ metdb_retrieval/python/get_data.py
@@ -7,9 +7,12 @@
 # USAGE         : get_data.py -c config.cfg
 #
 # REVISION INFO :
+# MB-1891: Jan 2019 Cater for retrieval periods of > 1 hour and for
+#                   "days ago" retrievals, both needed for AIRQAL and
+#                   AIRQALEU ServiceHub retrievals.                 AM
 # MB-1876: Dec 2018 Speed up CSV output by turning off type checking.
 # MB-1804: Nov 2018 Move metdb module corrections out of this source.
-#          Suppress printing of elements list unless in test mode.  SN      
+#          Suppress printing of elements list unless in test mode.  SN
 # MB-1824: Oct 2018 Add raw report text to METAR/SPECI              SN
 # MB-1803: Oct 2018 Use a csv dialect for output to get the quoting
 #          right for string containing commas.                      SN
@@ -33,6 +36,7 @@
 import ConfigParser
 import csv
 import datetime as dt
+from datetime import timedelta
 import getopt
 import importlib
 import metdb
@@ -332,7 +336,7 @@ class Elements():
 
 
 # ----------------------------------------------------------------------
-def time_from_ref(ref, hour=None, start=True):
+def time_from_ref(ref, days_ago=0, hour=None, start=True, time_span=None):
     """Create a MetDB format date/time string.
 
        Ref    - is a datetime object.
@@ -340,10 +344,11 @@ def time_from_ref(ref, hour=None, start=True):
        start  - is optional, default is True.
        when it is a start time then the minutes start at 00,
        otherwise an end time is assumed and minutes is set to 59.
-       If the requested hour is in the future then it uses the
-       day before the ref date instead.
+       If the requested hour is in the future and we are not retrieving from
+       any "days ago" then it uses the day before the ref date instead.
     """
     copy_ref = ref
+
     if hour is None:
         hr = ref.hour
         min = ref.minute
@@ -353,8 +358,10 @@ def time_from_ref(ref, hour=None, start=True):
             min = 0
         else:
             min = 59
-    if ref.hour < hr:
-        copy_ref = copy_ref - dt.timedelta(days=1)
+
+    if ref.hour < hr and days_ago == 0:
+        copy_ref = copy_ref - dt.timedelta(days=(1))
+
     return copy_ref.strftime("%Y%m%d/") + \
         '{:02d}{:02d}'.format(hr, min) + 'Z'
 
@@ -524,6 +531,35 @@ def get_data():
     if len(hour_list) == 0:
         hour_list = [now.hour]
 
+# time_span (number of hours for the retrieval) must be a single positive int.
+# Defaults to a span of 1 hour
+
+    time_span = 1
+    if hasattr(settings, 'time_span'):
+        try:
+            time_span = int(settings.time_span)
+        except:
+            print 'Invalid time_span value:', settings.time_span
+            sys.exit(2)
+        if time_span < 0:
+            print 'Invalid time_span (must be positive integer number of hours):', settings.time_span
+            sys.exit(2)
+
+# days_ago (number of whole days previously for the retrieval) must be a single positive int
+# Defaults to 0 days ago, i.e. today
+
+    days_ago = 0
+    if hasattr(settings, 'days_ago'):
+        try:
+            days_ago = int(settings.days_ago)
+        except:
+            print 'Invalid days_ago value:', settings.days_ago
+            sys.exit(2)
+        if days_ago < 0:
+            print 'Invalid days_ago (must be positive number of days):', settings.days_ago
+            sys.exit(2)
+
+
 # Get platform list, if any
 
     if hasattr(settings, 'platform'):
@@ -538,8 +574,8 @@ def get_data():
 
     for t in hour_list:
         csv_list = []  # list of lines to output
-        start_time = time_from_ref(now, hour=t, start=True)
-        end_time = time_from_ref(now, hour=t, start=False)
+        start_time = time_from_ref(now - timedelta(days=days_ago), days_ago, hour=t)
+        end_time = time_from_ref(now - timedelta(days=days_ago), days_ago, hour=(t+time_span-1), start=False, time_span=time_span)
         rcpt_time = time_from_ref(last_run)
         ob_dt = '{:02d}'.format(t)
 
