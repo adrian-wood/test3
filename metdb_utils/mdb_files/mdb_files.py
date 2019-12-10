e1707ea30cd4cf83e771471ef5f4c2707f66383d
Andy Moorhouse <andrew.moorhouse@metoffice.gov.uk>
2019-12-10 16:15:46 +0000

    MB-2030 2nd version :-)

--- metdb_utils/mdb_files/mdb_files.py
+++ metdb_utils/mdb_files/mdb_files.py
@@ -1,7 +1,80 @@
-class RetrievalTableEntry:
+class RetrievalTable:
+    def __init__(self, rt_file=None):
+        if rt_file is None:
+            self.headers = []
+            self.datatypes = {}
+            self.footers = []
+        else:
+            _rtlines = rt_file.readlines()
+            self.headers = _rtlines[:7]
+            for _count, _line in enumerate(_rtlines[7:]):
+                if _line.strip():
+                    datatype, dataset = self.unpack_line(_line)
+                    if datatype not in self.datatypes:  # new dict entry needed
+                        self.datatypes[datatype: None]
+                    self.datatypes[datatype].add_dataset(dataset)
+                else:
+                    break  # blank line so we're done
+            self.footers = _rtlines[(_count + 7):]
 
+    def unpack_line(self, line):
+        _datatype = line[1:9].strip()
+        _model = line[10:12].strip()
+        _raw_merged_flag = line[13:14].strip()
+        _start_time = line[15:31].strip()
+        _end_time = line[32:48].strip()
+        _storage_medium = line[49:50].strip()
+        _list_code_number = line[51:53].strip()
+        _elements_list = line[54:62].strip()
+        _elements_index = line[63:71].strip()
+        _retention_period_days = line[72:77].strip()
+        _index_records_per_dataset = line[78:82].strip()
+        _index_period_minutes = line[83:88].strip()
+        _record_length = line[89:95].strip()
+        _skeleton_flag = line[96:97].strip()
+        _mass_stream = line[98:101].strip()
+        _dataset_name = line[102:].strip()
+
+        _dataset = Dataset(_model,
+                           _raw_merged_flag,
+                           _start_time,
+                           _end_time,
+                           _storage_medium,
+                           _list_code_number,
+                           _elements_list,
+                           _elements_index,
+                           _retention_period_days,
+                           _index_records_per_dataset,
+                           _index_period_minutes,
+                           _record_length,
+                           _skeleton_flag,
+                           _mass_stream,
+                           _dataset_name)
+
+        return _datatype, _dataset
+
+    def list_datatypes(self):
+        return list(self.datatypes.keys())
+
+
+class Datatype:
+
+    def __init__(self, datatype):
+        self.datasets = []
+        self.number_of_datasets = 0
+
+    def add_dataset(self, dataset):
+        self.number_of_datasets += 1
+        self.datasets.append(dataset)
+
+
+class Dataset:
+    """ This class represents a single Dataset entry line in a retrieval table.
+
+        Attributes:
+
+    """
     def __init__(self,
-                 datatype,
                  model,
                  raw_merged_flag,
                  start_time,
@@ -17,7 +90,6 @@ class RetrievalTableEntry:
                  skeleton_flag,
                  mass_stream,
                  dataset_name):
-        self.datatype = datatype
         self.model = model
         self.raw_merged_flag = raw_merged_flag
         self.start_time = start_time
@@ -33,56 +105,3 @@ class RetrievalTableEntry:
         self.skeleton_flag = skeleton_flag
         self.mass_stream = mass_stream
         self.dataset_name = dataset_name
-
-
-class RetrievalTable:
-    def __init__(self, rt_file):
-        self.entries = []
-        _rtlines = rt_file.readlines()
-        self.headers = _rtlines[:7]
-        for _count, _line in enumerate(_rtlines[7:]):
-            if _line.strip():
-                _datatype = _line[1:9].strip()
-                _model = _line[10:12].strip()
-                _raw_merged_flag = _line[13:14].strip()
-                _start_time = _line[15:31].strip()
-                _end_time = _line[32:48].strip()
-                _storage_medium = _line[49:50].strip()
-                _list_code_number = _line[51:53].strip()
-                _elements_list = _line[54:62].strip()
-                _elements_index = _line[63:71].strip()
-                _retention_period_days = _line[72:77].strip()
-                _index_records_per_dataset = _line[78:82].strip()
-                _index_period_minutes = _line[83:88].strip()
-                _record_length = _line[89:95].strip()
-                _skeleton_flag = _line[96:97].strip()
-                _mass_stream = _line[98:101].strip()
-                _dataset_name = _line[102:].strip()
-                _entry = RetrievalTableEntry(_datatype,
-                                             _model,
-                                             _raw_merged_flag,
-                                             _start_time,
-                                             _end_time,
-                                             _storage_medium,
-                                             _list_code_number,
-                                             _elements_list,
-                                             _elements_index,
-                                             _retention_period_days,
-                                             _index_records_per_dataset,
-                                             _index_period_minutes,
-                                             _record_length,
-                                             _skeleton_flag,
-                                             _mass_stream,
-                                             _dataset_name)
-                self.entries.append(_entry)
-            else:
-                break  # blank line so we're done
-        self.footers = _rtlines[(_count + 7):]
-
-    def datatypes(self):
-        #   _datatypes = []
-        #   for _entry in self.entries:
-        #       _datatypes.append(_entry.datatype)
-        #   return _datatypes
-        _datatypes = [_entry.datatype for _entry in self.entries]
-        return _datatypes
