9beb64768de414a11c5f6be1ec76f6d2adfb9dee
Moorhouse, Andrew <andrew.moorhouse@metoffice.gov.uk>
2020-03-10 09:09:46 +0000

    retbadc.sh edited online with Bitbucket

--- BADC/cylc/scripts/retbadc.sh
+++ BADC/cylc/scripts/retbadc.sh
@@ -16,7 +16,7 @@ fi
 export METDB_SERVER_NUMBER=33556618
 export METDB_FREEPN_NUMBER=33556619
 export METDB_SERVER_IPNAME=mdbapop-prod
-export METDB_CLIENT_CONTACT=metdb@metoffice.gov.uk
+export METDB_CLIENT_CONTACT=metdb_badc@metoffice.gov.uk
 
 # Optional environment variables
 export METDB_RPC_TIMEOUT=5400
