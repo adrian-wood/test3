38e0e85a528436c268916bb481d1952e04a22f40
Andy Moorhouse <andrew.moorhouse@metoffice.gov.uk>
2020-05-06 12:52:26 +0000

    MB-18 Forgot TestPlan.txt

--- regression_testing/runner/scripts/daily_run.sh
+++ regression_testing/runner/scripts/daily_run.sh
@@ -2,7 +2,7 @@
 module load scitools
 module display scitools
 #
-# this bit just for testing or running from the command line - env will normally be set by cylc
+# this bit just for testing - env will be set by cylc
 #set -x
 #setenv=/home/sneedham/cylc-run/regression/set_env.rc
 #source $setenv
