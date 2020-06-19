7d1c61900ba615dbd2b501f273ca852d5b83b627
Richard Weedon <itwr@vld336.cmpd1.metoffice.gov.uk>
2020-06-19 12:03:46 +0100

    MB-1336 Revised to include JINJA output

--- codes_register/MetDB_cl.sh
+++ codes_register/MetDB_cl.sh
@@ -5,7 +5,7 @@
 #
 # Program:          MetDB_cl.sh
 #
-# Date:             18/02/2018
+# Date:             14 Aug 2019
 #
 # Purpose:          Intiate and run MetDB_Code_listing.py. This will update 
 #                   the MetDB codes register.
@@ -16,9 +16,13 @@
 #
 # create the mdb-repo name with the date timestamp appended to the end
 #
-mdrp="/tmp/metdb-repo"
+mdrp="/var/tmp/metdb-repo-"
 mdt=$(date +"%Y%m%d_%H%M%S")
 expd=$mdrp$mdt
+#
+expdout="http://www-mdb-apps-test/code_dep/output/"
+expdin="/var/www/html/code_dep/output/"
+link_files="/var/moods/code_register/template_ref/"  
 #
 # create git command to clone repository
 #
@@ -26,6 +30,6 @@ GIT_CMD="git clone ssh://git@exxgitrepo:7999/mood/metdb.git "$expd
 #
 $GIT_CMD
 #
-# run python script with env variable $MDPRF as input parameter
 #
-python MetDB_Code_listingt1.py $expd
+python MetDB_Code_listing.py $expd $expdout $link_files $expdin
+#
